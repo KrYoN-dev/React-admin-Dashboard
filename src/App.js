52e07cc079d6ee481ba5ec1151872ef001887ae0
kryon22 <shubhamkharabe02@gmail.com>
2021-09-24 16:30:25 +0530

    created sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import Sidebar from "./component/sidebar/Sidebar";
 import Topbar from "./component/topbar/Topbar";
+import "./app.css"
 
 
  function App() {
     return (
         <div>
         <Topbar/>
+        <div className="container">
+            <Sidebar/>
+            <div className="others">
+                others
+                
+            </div>
+            
+        </div>
+
             
         </div>
     )
