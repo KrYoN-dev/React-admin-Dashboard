b7ede84dfd2a33973fd5c3df1f75614f1adb8460
kryon22 <shubhamkharabe02@gmail.com>
2021-09-29 17:36:07 +0530

    added the userList componet

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import Sidebar from "./component/sidebar/Sidebar";
 import Topbar from "./component/topbar/Topbar";
 import "./app.css"
 import Home from "./pages/home/Home" ;
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+  } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 
  function App() {
     return (
-        <div>
+        <Router>
         <Topbar/>
         <div className="container">
             <Sidebar/>
+            <Switch>
+            <Route exact path="/">
             <Home/>
+             </Route>
+             <Route path="/users">
+            <UserList/>
+             </Route>
+            
+
+            </Switch>
+            
             
         </div>
 
             
-        </div>
+        </Router>
     )
 }
 
--- src/dummyData.js
+++ src/dummyData.js
@@ -48,3 +48,78 @@ export const userData = [
       "Active User": 3000,
     },
   ];
+
+  export const UserRows = [
+    { id: 1, 
+        userName: 'Jhon Snow',
+       avatar : "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", 
+        email: "hon@gmal.com",
+        status:"active",
+        transcation:"$120.00" 
+    },
+    { id: 2, 
+        userName: 'Jhon Snow',
+        avatar:"https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "hon@gmal.com",
+        status:"active",
+        transcation:"$120.00" 
+    },
+    { id: 3, 
+        userName: 'Jhon Snow',
+        avatar: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", 
+        email: "hon@gmal.com",
+        status:"active",
+        transcation:"$120.00" 
+    },
+    { id: 4, 
+        userName: 'Jhon Snow',
+        avatar: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", 
+        email: "hon@gmal.com",
+        status:"active",
+        transcation:"$120.00" 
+    },
+    { id: 5, 
+        userName: 'Jhon Snow',
+        avatar: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", 
+        email: "hon@gmal.com",
+        status:"active",
+        transcation:"$120.00" 
+    },
+    { id: 6, 
+        userName: 'Jhon Snow',
+        avatar: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", 
+        email: "hon@gmal.com",
+        status:"active",
+        transcation:"$120.00" 
+    },
+    { id: 7, 
+        userName: 'Jhon Snow',
+        avatar: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", 
+        email: "hon@gmal.com",
+        status:"active",
+        transcation:"$120.00" 
+    },
+    { id: 8, 
+        userName: 'Jhon Snow',
+        avatar: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", 
+        email: "hon@gmal.com",
+        status:"active",
+        transcation:"$120.00" 
+    },
+    { id: 9, 
+        userName: 'Jhon Snow',
+        avatar: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", 
+        email: "hon@gmal.com",
+        status:"active",
+        transcation:"$120.00" 
+    },
+    { id: 10, 
+        userName: 'Jhon Snow',
+        avatar: "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80", 
+        email: "hon@gmal.com",
+        status:"active",
+        transcation:"$120.00" 
+    },
+
+    
+  ];
